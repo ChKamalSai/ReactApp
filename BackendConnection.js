989bc2a53fa300997595deecaa74a74ac6cddc50
ChKamalSai <156399814+ChKamalSai@users.noreply.github.com>
2024-04-06 16:02:51 +0530

    Update and rename Connection.js to BackendConnection.js

--- BackendConnection.js
+++ BackendConnection.js
@@ -1,8 +1,8 @@
 const mySql = require("mysql2")
 const connection = mySql.createPool({
     host: "localhost",
-    user: "root",
-    password: "Kamal@7989",
+    user: ,
+    password: ,
     database: "GMT"
 })
 module.exports=connection
